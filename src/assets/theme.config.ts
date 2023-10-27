08bd3b7cfc1865f2eca2d85149e8f71bdb174606
l.forzutti <l.forzutti@egeatech.com>
2023-10-27 21:44:29 +0200

    chore(theme): config

---
+++ src/assets/theme.config.ts
@@ -0,0 +1,31 @@
+export const MENU_ITEM_HEIGHT = 40;
+export const MOBILE_MAX_WIDTH_PX = 640;
+export const SIDENAV_WIDTH = "290px";
+
+export enum SIZE_OPTS {
+    XS,
+    SM,
+    MD,
+    LG,
+}
+
+export enum COLOR_OPTS {
+    PRIMARY = "indigo",
+    DANGER = "red",
+    WARNING = "orange",
+    SUCCESS = "emerald",
+}
+
+export enum TYPE_OPTS {
+    INFO = "info",
+    WARNING = "warning",
+    DANGER = "danger",
+    SUCCESS = "success",
+}
+
+export const CONTROL_SIZES = {
+    XS: 7,
+    SM: 9,
+    MD: 11,
+    LG: 14,
+};
